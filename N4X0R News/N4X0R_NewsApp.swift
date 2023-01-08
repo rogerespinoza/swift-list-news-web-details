fce086bea9293934fc07f9af887b276d8defe56b
Roger Espinoza <rj.espinoza44@gmail.com>
2023-01-08 17:44:57 -0500

    Initial Commit

---
+++ N4X0R News/N4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  N4X0R_NewsApp.swift
+//  N4X0R News
+//
+//  Created by Roger Espinoza on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct N4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
